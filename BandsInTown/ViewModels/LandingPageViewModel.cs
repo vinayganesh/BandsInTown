63c22ad326f18f42e79077828d8d5d3933dd0816
vinayganeshh <vinayganeshh@gmail.com>
2015-05-07 22:18:34 -0700

    Search Funtionality on Landing Page added

--- BandsInTown/ViewModels/LandingPageViewModel.cs
+++ BandsInTown/ViewModels/LandingPageViewModel.cs
@@ -4,29 +4,55 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.System;
 using Windows.UI.Popups;
+using Windows.UI.Xaml.Input;
 using BandsInTown.IServices;
 using BandsInTown.Models;
 using Caliburn.Micro;
 
 namespace BandsInTown.ViewModels
 {
-    public class LandingPageViewModel
+    public class LandingPageViewModel:PropertyChangedBase
     {
         private readonly ISearchArtistService _searchArtistService;
 
+        private string _artistName;
+
+        //Dependency Injection
         public LandingPageViewModel(ISearchArtistService searchArtistService)
         {
             _searchArtistService = searchArtistService;
         }
 
-        public async void SearchArtist()
+        //this is used for binding data
+        public string ArtistName
         {
-            var artist = await _searchArtistService.SearchArtist("Slayer");
-            MessageDialog md = new MessageDialog(artist.upcoming_event_count);
-            md.ShowAsync();
+            get { return _artistName; }
+            set
+            {
+                _artistName = value;
+                NotifyOfPropertyChange(() => ArtistName);
+            }
         }
 
-        
+        //Search for artists
+        public async void SearchArtist(ActionExecutionContext context)
+        {
+            try
+            {
+                var eventArgs = context.EventArgs as KeyRoutedEventArgs;
+                if (eventArgs != null && eventArgs.Key == VirtualKey.Enter)
+                {
+                    var artist = await _searchArtistService.SearchArtist(ArtistName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            
+        }
+
     }
 }
