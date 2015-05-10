b4eda94c2062592f5d1e501582ea523c5d78f84f
vinayganeshh <vinayganeshh@gmail.com>
2015-05-09 22:24:17 -0700

    Added Backgrounder

---
+++ Backgrounder/TimerTriggerTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace Backgrounder
+{
+    class TimerTriggerTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            //Add the logic to poll to get new events
+        }
+    }
+}
