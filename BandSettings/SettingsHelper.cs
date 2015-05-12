0fe98ecbddfc18b5c92f081f0320e853507a00fc
vinayganeshh <vinayganeshh@gmail.com>
2015-05-11 23:33:40 -0700

    Added settings helper

---
+++ BandSettings/SettingsHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace BandSettings
+{
+    public sealed class SettingsHelper
+    {
+        private static readonly object Locker = new object();
+        public static T ParseEnum<T>(string k)
+        {
+            lock (Locker)
+            {
+                var settings = ApplicationData.Current.LocalSettings;
+                var v = settings.Values[k];
+                if (v == null)
+                    return default(T);
+
+                return (T)Enum.Parse(typeof(T), v.ToString());
+            }
+        }
+
+        public static void SetValue<T>(string k, T v)
+        {
+            try
+            {
+                lock (Locker)
+                {
+                    var settings = ApplicationData.Current.LocalSettings;
+                    settings.Values[k] = v;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Can't set settings value for '{0}': {1}", k, ex);
+            }
+        }
+
+        public static T GetValue<T>(string k)
+        {
+            lock (Locker)
+            {
+                var settings = ApplicationData.Current.LocalSettings;
+                var v = settings.Values[k];
+                if (v == null)
+                    return default(T);
+                try
+                {
+                    return (T)v;
+                }
+                catch (InvalidCastException)
+                {
+                    Debug.WriteLine("Can't get settings value for '{0}'", k);
+                    return default(T);
+                }
+            }
+        }
+    }
+}
---
+++ BandsInTown/Helpers/BackgroundTask/BackgroundTaskHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace BandsInTown.Helpers.BackgroundTask
+{
+    internal class CommonBackgroundTaskHelper
+    {
+        private const string EntryPoint = "Backgrounder.TimerTriggerTask";
+        private const string TaskName = "TimerTriggerTask";
+        private const int TimeInterval = 30; // In minutes.
+
+        public async Task Register()
+        {
+            BackgroundTaskConfiguration.UnregisterBackgroundTasks(TaskName);
+
+            await BackgroundTaskConfiguration.RegisterBackgroundTask(EntryPoint, TaskName, new TimeTrigger(TimeInterval, false),
+                new SystemCondition(SystemConditionType.InternetAvailable));
+        }
+
+        public IBackgroundTaskRegistration Task
+        {
+            get
+            {
+                return BackgroundTaskRegistration.AllTasks.Select(kv => kv.Value).SingleOrDefault(i => i.Name == TaskName);
+            }
+        }
+    }
+}
